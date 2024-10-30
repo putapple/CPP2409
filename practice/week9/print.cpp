f46b7b382d4e5c228c139f9df93bee739bedc448
putapple <qwr7604@naver.com>
2024-10-30 15:51:01 +0900

    Class Exercises

---
+++ practice/week9/print.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class PrintData {
+public:
+    void print(int i) {cout << i << endl; }
+    void print(double f) {cout << f << endl;}
+    void print(string s = "No Data!") {cout << s <<endl; }
+};
+
+
+int main() {
+    PrintData obj;
+
+    obj.print(1);
+    obj.print(3.14);
+    obj.print("C++14 is cool.");
+    obj.print();
+    return 0;
+}
